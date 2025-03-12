d8ee31cd30d9eea8937b8a562f18a95f609f6170
rahulpoojith <poojith.p.rahul@gmail.com>
2025-03-12 06:54:23 +0000

    12 Mar 2025, 12:24

--- app.py
+++ app.py
@@ -4,6 +4,30 @@ import tensorflow as tf
 from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 import pandas as pd
 import pickle
-
-## Load the trained model
+# Load the trained model, scaler, and encoders
 model = tf.keras.models.load_model('model.h5')
+
+# Load the encoders and scaler
+with open('label_encoder_gender.pkl', 'rb') as file:
+    label_encoder_gender = pickle.load(file)
+
+with open('one_hot_enc_geo.pkl', 'rb') as file:
+    one_hot_enc_geo = pickle.load(file)  # Use the correct variable name
+
+with open('scaler.pkl', 'rb') as file:
+    scaler = pickle.load(file)
+
+
+## Streamlit app
+st.title('Bank Customer Churn Prediction')
+# User input
+geography = st.selectbox('Geography', onehot_encoder_geo.categories_[0])
+gender = st.selectbox('Gender', label_encoder_gender.classes_)
+age = st.slider('Age', 18, 92)
+balance = st.number_input('Balance')
+credit_score = st.number_input('Credit Score')
+estimated_salary = st.number_input('Estimated Salary')
+tenure = st.slider('Tenure', 0, 10)
+num_of_products = st.slider('Number of Products', 1, 4)
+has_cr_card = st.selectbox('Has Credit Card', [0, 1])
+is_active_member = st.selectbox('Is Active Member', [0, 1])
